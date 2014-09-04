30c28bbb16511a6f6902f1909455e53f516c8996
Xiaohui Chen <xhchen0328@utexas.edu>
2014-09-04 00:03:24 -0500

    finish bash script for part 1

---
+++ p1/run.script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+echo Start executing part 1
+
+cd part1/myfsm
+
+M2T="java CatCore.vm2t.Main fsm.pl myfsm.vm"
+
+echo $M2T
+
+$M2T
+
+cd ..
+
+javac="javac app.java"
+echo $javac
+
+java="java app"
+
+echo "java app"
+echo The execution result of part 1:
+echo
+$java
+
