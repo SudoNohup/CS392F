f37c2f334d2b26a4dbce7c976c5d3f9b2e18e0c7
Xiaohui Chen <xhchen0328@utexas.edu>
2014-09-07 15:14:32 -0500

    commit before modification

---
+++ p1/part2/myfsm/fsm.java
@@ -0,0 +1,68 @@
+package myfsm;
+
+public class fsm {
+  public enum states { start, ready, drink, eat, family, stop }
+  states currentState = states.start;
+  public String getName() { return currentState.toString(); }
+  public void gotostart() {  
+    switch(currentState) {
+    default : 
+      System.out.println("ignoring transition to start");
+	}
+  }
+  public void gotoready() {  
+    switch(currentState) {
+    case start : 
+      System.out.println("going to ready");
+      currentState = states.ready;
+      break;
+    default : 
+      System.out.println("ignoring transition to ready");
+	}
+  }
+  public void gotodrink() {  
+    switch(currentState) {
+    case ready : 
+    case drink : 
+    case eat : 
+      System.out.println("going to drink");
+      currentState = states.drink;
+      break;
+    default : 
+      System.out.println("ignoring transition to drink");
+	}
+  }
+  public void gotoeat() {  
+    switch(currentState) {
+    case ready : 
+    case eat : 
+    case drink : 
+      System.out.println("going to eat");
+      currentState = states.eat;
+      break;
+    default : 
+      System.out.println("ignoring transition to eat");
+	}
+  }
+  public void gotofamily() {  
+    switch(currentState) {
+    case drink : 
+    case eat : 
+      System.out.println("going to family");
+      currentState = states.family;
+      break;
+    default : 
+      System.out.println("ignoring transition to family");
+	}
+  }
+  public void gotostop() {  
+    switch(currentState) {
+    case family : 
+      System.out.println("going to stop");
+      currentState = states.stop;
+      break;
+    default : 
+      System.out.println("ignoring transition to stop");
+	}
+  }
+}
