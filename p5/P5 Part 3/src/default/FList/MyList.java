56632c90533b46b4c071047e3eaec0694f013375
Xiaihui Chen <xhchen0328@utexas.edu>
2014-10-11 17:30:36 -0500

    commit something

---
+++ p5/P5 Part 3/src/default/FList/MyList.java
@@ -0,0 +1,57 @@
+package FList; 
+
+import java.io.PrintStream; 
+import LApp.Entity; 
+import java.util.Iterator; 
+
+public  class  MyList  implements Iterable {
+	
+
+    MyNode head;
+
+	
+    MyNode tail;
+
+	
+
+    public MyList() {
+        head = null;
+        tail = null;
+    }
+
+	
+
+    public void insert(Entity elem) {
+        insert(new MyNode(elem));
+    }
+
+	
+
+    void insert(MyNode n) {
+        n.right = head;
+        head = n;
+    }
+
+	
+
+    public Iterator iterator() {
+        return new MyIterator(this);
+    }
+
+	
+
+    public void print(PrintStream out) {
+        for (Iterator eIterator = iterator(); 
+		eIterator.hasNext();) {
+            Entity e = (Entity) eIterator.next();
+            out.println(e);
+        }
+    }
+
+	
+
+    public void delete(MyNode n) {
+    }
+
+
+}
---
+++ p5/P5 Part 3/src/default/FList/MyNode.java
@@ -0,0 +1,26 @@
+package FList; 
+
+
+public  class  MyNode {
+	
+
+    Object elem;
+
+	
+    MyNode right;
+
+	
+
+    public MyNode(Object elem) {
+        this.elem = elem;
+        right = null;
+    }
+
+	
+
+    public String toString() {
+        return elem.toString();
+    }
+
+
+}
